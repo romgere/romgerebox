eee0e12cb51623b1a23f6f419a4c9235aa477f2b
Romgere <jerome@mestres.fr>
2019-06-14 11:15:12 +0200

    Reorganisation of btn/controls and refactorisation of main-box component

---
+++ app/components/box-main-recordbox.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  classNames: ['flex','layout-row','layout-align-center-center'],
+  recordAction: function(){},
+  micAction: function(){},
+  downloadAction: function(){},
+  showDownload: false,
+});
---
+++ app/components/box-main-toolbar.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  playAction: function(){},
+  homeAction: function(){},
+  saveAction: function(){},
+});
