381c8dda85967fb42993560c7a4845a3918c8abe
romgere <jerome@mestres.Fr>
2021-12-09 22:03:20 +0100

    refactor: recordbox & toolbar to single component

---
+++ app/components/box-toolbar.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component'
+import Constants from 'romgerebox/constants'
+
+export default class BoxMainToolbarComponent extends Component {
+  loopInfo = Constants.LOOP_INFO_TYPE
+}
