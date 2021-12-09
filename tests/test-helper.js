b7fcb999d7c62474c741fb634673eb35c8d6a566
Romgere <jerome_mestres@ultimatesoftware.com>
2021-12-09 15:16:39 +0100

    build: update to ember 3.28.4

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'romgerebox/app';
+import config from 'romgerebox/config/environment';
+import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
 
 start();
