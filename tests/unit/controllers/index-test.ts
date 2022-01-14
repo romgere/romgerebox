1c1d450702458dbfec6acf39d644e71dae9491ec
Romgere <jerome_mestres@ultimatesoftware.com>
2022-01-14 09:37:05 +0100

    chore: convert route/controller test to ts files

--- tests/unit/controllers/index-test.ts
+++ tests/unit/controllers/index-test.ts
@@ -1,12 +1,11 @@
 import { module, test } from 'qunit'
 import { setupTest } from 'ember-qunit'
 
-module('Unit | Controller | box', function (hooks) {
+module('Unit | Controller | index', function (hooks) {
   setupTest(hooks)
 
-  // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let controller = this.owner.lookup('controller:box')
+    let controller = this.owner.lookup('controller:index')
     assert.ok(controller)
   })
 })
