da048b63b8eb3d5ce8406945959df500b2847719
Romgere <jerome_mestres@ultimatesoftware.com>
2022-02-01 17:22:44 +0100

    feat: add footer with some links

--- app/controllers/application.ts
+++ app/controllers/application.ts
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller'
 import { inject as service } from '@ember/service'
 import { action } from '@ember/object'
+import { htmlSafe } from '@ember/template'
 
 import type IntlService from 'ember-intl/services/intl'
 
@@ -11,6 +12,14 @@ export default class ApplicationController extends Controller {
   availableLocales = ['fr-fr', 'en-us']
   get currentLocale() {
     return this.intl.primaryLocale
+  }
+
+  get authorLink() {
+    return htmlSafe('<a href="https://github.com/romgere" target="_blank" rel="noopener noreferrer">Romgere</a>')
+  }
+
+  get emberLink() {
+    return htmlSafe('<a href="https://emberjs.com/" target="_blank" rel="noopener noreferrer">Ember.js</a>')
   }
 
   @action
